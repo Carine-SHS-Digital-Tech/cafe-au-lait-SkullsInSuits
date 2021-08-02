16ff067dc9c75964cb59561b755b70adeaf21308
Joseph O'Donnell <odoj23@gmail.com>
2021-08-02 11:49:56 +0800

    First real commit

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello WOrld")
+DineInQuantity = 0
+TakeAwayQuantity = 0
+TotalOrders = 0
+TotalCups = 0
+TotalDayIncome = 0
+TotalGst = 0
+CappuccinoQuantity = 0
+EspressoQuantity = 0
+LatteQuantity = 0
+IcedCoffeeQuantity = 0
+OpType = 1
+OpType = input("Press 1 for New Order, press 0 for Daily Summary: ")
+if OpType = 0:
+    print(DineInQuantity)
+    print(TakeAwayQuantity)
+    print(TotalOrders)
+    print(TotalCups)
+    print(TotalDayIncome)
+    print(TotalGst)
+    print(CappuccinoQuantity)
+    print(EspressoQuantity)
+    print(LatteQuantity)
+    print(IcedCoffeeQuantity)
+else:
+    print("You doingo")
